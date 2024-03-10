e9f93c711fa6525c79b2021750ebe0cebc35a00c
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-10 21:15:27 +0300

    add console log statement instead of a return statement

---
+++ assets/rps.js
@@ -0,0 +1,18 @@
+let randomNumber = Math.random();
+
+if (randomNumber > 0 && randomNumber < 1 / 3){
+    console.log('rock');
+
+}
+
+else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+
+    console.log('rock');
+
+}
+
+else if (randomNumber >= 2 / 3 && randomNumber < 1){
+    console.log('rock');
+}
+
+console.log(randomNumber);
