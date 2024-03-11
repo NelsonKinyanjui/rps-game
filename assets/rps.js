3b662fd5875b690eb63ce508984f840dbaebd5f6
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-11 05:41:25 +0300

    add  return statement on results and game score

--- assets/rps.js
+++ assets/rps.js
@@ -20,10 +20,48 @@ else if (randomNumber >= 2 / 3 && randomNumber < 1){
 }
 }
 
-console.log(getComputerChoice());
+let playerScore = 0;
+let computerScore = 0;
 
 
 function playRound(playerSelection , computerSelection){
+    if (playerSelection === 'rock' && computerSelection === 'scissors' ){
+        playerScore ++;
+        return `You Win ${playerSelection} beats ${computerSelection}.`
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors' ){
+        computerScore ++;
+        return `You lose ${computerSelection} beats ${playerSelection}.`
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'scissors' ){
+    
+        return `That's a Tie.`;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper' ){
+        computerScore ++;
+        return `You lose ${computerSelection} beats ${playerSelection}.`
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'paper' ){
+        
+        return `That's a Tie.`;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper' ){
+        playerSelection ++;
+        return `You Win ${playerSelection} beats ${computerSelection}.`
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'rock' ){
+        
+        return `That's a Tie`;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock' ){
+        computerScore ++;
+        return `You lose ${computerSelection} beats ${playerSelection}.`
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock' ){
+
+        playerSelection ++;
+        return `You win ${playerSelection} beats ${computerSelection}`;
+    }
    
 }
 
